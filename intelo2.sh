c11de2c0eec7e4113a76b88266fc12841f2186ae
root <hermann.sky@gmail.com>
2018-07-21 11:56:07 -0400

    daily update

--- intelo2.sh
+++ intelo2.sh
@@ -8,7 +8,7 @@
 espeak -v female5 -p 50 -s 150  "What do you want. me. to lookup for you on Wikipedia."
 echo "What do you want me to lookup for you on Wikipedia ?"; \
 read line; \
-w3m http://en.wikipedia.org/wiki/"${line}"> text.txt
-espeak -v female5 -p 50 -s 150 -f text.txt
-rm -rf text.txt
+w3m http://en.wikipedia.org/wiki/"${line}"> "${line}".itl
+espeak -v female5 -p 50 -s 150 -f "${line}".itl
+#rm -rf text.itl
 
--- voice_date.sh
+++
@@ -1,4 +0,0 @@
-
-date>date.txt
-espeak -v female5 -f date.txt
-
